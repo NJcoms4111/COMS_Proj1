e4d4432dd5f7f9bda267d4fd612b0b7bf67acea3
Joy Parikh <52308264+joyparikh@users.noreply.github.com>
2022-04-15 08:57:20 -0400

    recent server

--- server.py
+++ server.py
@@ -20,8 +20,8 @@ from datetime import datetime, timezone
 from sqlalchemy.sql.functions import user
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-#app.secret_key = ("b'\xa1\xac9\x1f\xfa\x9a\xa9\xc4\xb5c\xdcr\xb0'" +
-#                           "b'\x96\xdc\xfb\xf5B\xd7\x0e\x17.\xe7'")
+app.secret_key = ("b'\xb9\xdc1\x2f\xfc\x1b\xb9\xa9\xb5d\xadr\xb0'" +
+                           "b'\x96\xdc\xfb\xf5B\xd7\x0e\x17.\xe7'")
 
 
 
@@ -116,26 +116,27 @@ def index():
   # check if user is logged in
   if 'logname' not in session:
     return redirect(url_for('login'))
-  
   cursor = g.conn
   # Get the real name of the logged in user
   name = cursor.execute("""
-    SELECT users.name
+    SELECT users.name,sid
     FROM users
-    WHERE username = %(username)s;
+    WHERE email = %(email)s;
     """, {
-      'username': session['logname']
+      'email': session['logname']
     })
-  name = name.fetchone()[0]
-
-  #
+  name = name.fetchone()
+  display=True
+  if name[1]==None:
+    display=False
+  print(display)
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
+  #cursor = g.conn.execute("SELECT name FROM test")
+  #names = []
+  #for result in cursor:
+  #  names.append(result['name'])  # can also be accessed using result[0]
+  #cursor.close()
 
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
@@ -163,7 +164,7 @@ def index():
   #     <div>{{n}}</div>
   #     {% endfor %}
   #
-  context = dict(data = names)
+  context = dict(data = [name[0],display])
 
 
   #
@@ -180,9 +181,150 @@ def index():
 # notice that the functio name is another() rather than index()
 # the functions for each app.route needs to have different names
 #
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+# check if user is logged in
+  if 'logname' not in session:
+    return redirect(url_for('login'))
+  if request.method == 'GET':
+    cursor = g.conn
+    # Get the real name of the logged in user
+    cart_data = cursor.execute("""
+      select sell_item.name,condition,price,users.name as seller_name,category.name as category 
+      from has,sell_item,users,belong,Category 
+      where has.usr_email = %(email)s and sell_item.iid = has.iid and has.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid
+      """, {
+        'email': session['logname']
+      })
+    cart_data = cart_data.fetchall()
+  context = dict(cart_data = cart_data)
+  return render_template("cart.html",**context)
+
+
+@app.route('/browse_items', methods=['GET', 'POST'])
+def browse_items():
+  if 'logname' not in session:
+    return redirect(url_for('login'))
+  #Sort by price-a - default
+  cursor = g.conn
+  # Get the real name of the logged in user
+  selector = list(request.form.values()) or ['0']
+  if selector[0] == '0':
+    item_data = cursor.execute("""
+      select sell_item.name,price,condition,users.name as seller_name,Category.name as category 
+      from sell_item,users,belong,Category 
+      where sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid
+      except
+      select sell_item.name,price,condition,users.name as seller_name,Category.name as category 
+      from sell_item,users,belong,Category,buy 
+      where sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid and sell_item.iid=buy.iid 
+      order by price asc 
+      limit 15
+      """)
+    rows = 5
+  if selector[0] == '1':
+    item_data = cursor.execute("""
+      select sell_item.name,price,condition,users.name as seller_name,Category.name as category 
+      from sell_item,users,belong,Category 
+      where sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid 
+      except
+      select sell_item.name,price,condition,users.name as seller_name,Category.name as category 
+      from sell_item,users,belong,Category,buy
+      where sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid and sell_item.iid=buy.iid 
+      order by price desc 
+      limit 15
+      """)
+    rows = 5
+  if selector[0] == '3':
+    item_data = cursor.execute("""
+    select sell_item.name,price,condition,users.name as seller_name,Category.name as category,counts.pop as followers
+    from (
+      select count(*)as pop,usr2_email from follow group by usr2_email) as counts,sell_item,users,belong,Category 
+    where counts.usr2_email=users.email and users.sid=sell_item.sid and sell_item.iid=belong.iid and category.caid=belong.caid 
+    except
+    select sell_item.name,price,condition,users.name as seller_name,Category.name as category,counts.pop as followers
+    from (
+      select count(*)as pop,usr2_email from follow group by usr2_email) as counts,sell_item,users,belong,Category,buy 
+    where counts.usr2_email=users.email and users.sid=sell_item.sid and sell_item.iid=belong.iid and category.caid=belong.caid and sell_item.iid=buy.iid 
+    order by followers desc 
+    limit 15
+    """)
+    rows = 6
+  if selector[0] == '2':
+    item_data = cursor.execute("""
+    CREATE OR REPLACE FUNCTION custom_sort(anyarray, anyelement)
+    RETURNS INT AS 
+    $$
+     SELECT i FROM (
+     SELECT generate_series(array_lower($1,1),array_upper($1,1))
+     ) g(i)
+    WHERE $1[i] = $2
+    LIMIT 1;
+    $$ LANGUAGE SQL IMMUTABLE;
+
+      SELECT *
+   FROM (
+     SELECT sell_item.name,price,condition,users.name as seller_name,Category.name as category
+   FROM sell_item,users,belong,Category
+   WHERE sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid
+   except
+   SELECT sell_item.name,price,condition,users.name as seller_name,Category.name as category
+   FROM sell_item,users,belong,Category,buy
+   WHERE sell_item.sid=users.sid and sell_item.iid=belong.iid and category.caid=belong.caid and sell_item.iid=buy.iid) as tmp
+   ORDER BY custom_sort(ARRAY['Like New', 'Good', 'Fair', 'Bad'], tmp.condition)
+   LIMIT 15
+   """)
+    rows = 5
+    print(item_data)
+  context = dict(item_data = item_data)
+  return render_template("browse_items.html",**context)
+
+@app.route('/sellitems',methods=['GET', 'POST'])
+def sellitems():
+  if 'logname' not in session:
+    return redirect(url_for('index'))
+  if 'logsid' not in session:
+    return redirect(url_for('index'))
+  cursor = g.conn
+  sell_data = cursor.execute("""
+    select name,price,condition from sell_item where sid=%(sid)s
+    except
+    select name,price,condition from sell_item,buy where sell_item.iid=buy.iid and sell_item.sid=%(sid)s
+    """,{
+    'sid':session['logsid']
+    })
+  context = dict(sell_data = sell_data)
+  return render_template("sellitems.html",**context)
+
+@app.route('/solditems',methods=['GET', 'POST'])
+def solditems():
+  if 'logname' not in session:
+    return redirect(url_for('index'))
+  if 'logsid' not in session:
+    return redirect(url_for('index'))
+  cursor = g.conn
+  sold_data = cursor.execute("""
+    select name,price,condition from sell_item,buy where sell_item.iid=buy.iid and sell_item.sid=%(sid)s
+    """,{
+    'sid':session['logsid']
+    })
+  context = dict(sold_data = sold_data)
+  return render_template("solditems.html",**context)
+
+@app.route('/bought_item')
+def bought_item():
+  if 'logname' not in session:
+    return redirect(url_for('index'))
+  if request.method == 'GET':
+    cursor = g.conn
+    bought_data = cursor.execute("""
+    select name,price,condition,carrier,eta,status 
+    from buy,sell_item,Delivery,logistics 
+    where usr_email=%(email)s and delivery.did=logistics.did and logistics.iid=buy.iid and sell_item.iid=buy.iid
+    """,{
+      'email': session['logname']})
+  context = dict(bought_data = bought_data)
+  return render_template("bought_item.html",**context)
 
 # Password page
 @app.route('/password', methods=['GET', 'POST'])
@@ -194,15 +336,15 @@ def password():
     new_password = request.form['new_password']
     cursor = g.conn
     exists = cursor.execute("""
-    SELECT users.username AS username FROM users
-    WHERE username = %(username)s
+    SELECT users.email AS email FROM users
+    WHERE email = %(email)s
     AND password = %(password)s;
     """, {
-      'username': session['logname'],
+      'email': session['logname'],
       'password': old_password
     })
     exists = exists.fetchall()
-    # Throw error 403 if no such username/password combination
+    # Throw error 403 if no such email/password combination
     if not exists:
       cursor.close()
       abort(403)
@@ -210,10 +352,10 @@ def password():
     cursor.execute("""
     UPDATE users
     SET password = %(password)s
-    WHERE username = %(username)s;
+    WHERE email = %(email)s;
     """, {
       'password': new_password,
-      'username': session['logname']
+      'email': session['logname']
     })
     return redirect(url_for('index'))
   return render_template("password.html")
@@ -225,47 +367,78 @@ def login():
   if 'logname' in session:
     return redirect(url_for('index'))
   
-  # Check whether username/password combination can be found
+  # Check whether email/password combination can be found
   if request.method == 'POST':
     exists = None
-    input_username = request.form['username']
+    input_email = request.form['email']
     input_password = request.form['password']
+    input_sid = request.form['sid']
     cursor = g.conn
-    exists = cursor.execute("""
-    SELECT users.username AS username FROM users
-    WHERE username = %(username)s
-    AND password = %(password)s;
-    """, {
-      'username': input_username,
-      'password': input_password
-    })
+
+    if input_sid != '':
+      exists = cursor.execute("""
+      SELECT users.email AS email FROM users
+      WHERE email = %(email)s
+      AND password = %(password)s
+      AND sid = %(sid)s;
+      """, {
+        'email': input_email,
+        'password': input_password,
+        'sid':input_sid
+      })
+    else:
+      exists = cursor.execute("""
+      SELECT users.email AS email FROM users
+      WHERE email = %(email)s
+      AND password = %(password)s
+      """, {
+        'email': input_email,
+        'password': input_password,
+      })
     exists = exists.fetchall()
-    # Throw error 403 if no such username/password combination
+    print(exists)
+    # Throw error 403 if no such email/password combination
     if not exists:
       cursor.close()
       abort(403)
     
     # Create cookie if log in is successful
-    session['logname'] = input_username
+    session['logname'] = input_email
+    if input_sid != '':
+      session['logsid'] = input_sid
     cursor.close()
     return redirect(url_for('index'))
   return render_template("login.html")
 
 # Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
+@app.route('/additem', methods=['POST'])
+def additem():
+  if 'logname' not in session:
+    return redirect(url_for('index'))
+  if 'logsid' not in session:
+    return redirect(url_for('index'))
   name = request.form['name']
+  price = request.form['price']
+  condition = request.form['condition']
   print (name)
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
+  cursor = g.conn
+  exist = cursor.execute("""
+      SELECT distinct iid FROM sell_item
+      """)
+  exist = exist.fetchall()
+  iid = iid = randint(100000,999999)
+  while iid in exist : iid = randint(100000,999999)
+  cmd = 'INSERT INTO sell_item VALUES (:iid1,:name1,:price1,:condition1,:img1,:sid1)';
+  g.conn.execute(text(cmd), iid1 = iid,name1=name,price1=int(price),condition1=condition,img1='image12',sid1=int(session['logsid']));
   return redirect('/')
 
 
+
 # Lougout
-@app.route('/logout/', methods=['POST'])
+@app.route('/logout/', methods=['POST','GET'])
 def logout():
   session.clear()
-  return redirect(url_for('login'))
+  return render_template("login.html")
 
 
 if __name__ == "__main__":
